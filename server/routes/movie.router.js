b93bcd021fd5c2adeff1e0a4d06c1e00526c44bd
Padee Vang <vangpadee11@gmail.com>
2024-10-07 15:33:37 -0500

    fixed router get

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+router.get('/', (req, res) => {
+  const query = `
+    SELECT * FROM "movies"
+      ORDER BY "title" ASC;
+  `;
+  pool.query(query)
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all movies', err);
+      res.sendStatus(500)
+    })
+
+});
+
 router.get('/:id', (req, res) => {
   const movieId = req.params.id;
   const query = `
